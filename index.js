29fb248fa7dc6365d32434732112e1eacaa05857
parveen <parveen4900613@example.com>
2022-10-11 13:55:59 +0530

    make it better

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.get('/', (req, res)=> {
 
 const server = require('http').createServer(app);
 const io = require('socket.io')
-// (server, {
-//   cors: {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST']
-//   }
-// })
+(server, {
+  cors: {
+    origins: '*',
+    methods: ['GET', 'POST']
+  }
+})
 
 
 async function getLastMessagesFromRoom(room){
